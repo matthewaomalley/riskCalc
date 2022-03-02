018432475d5cb8cb9ac1891a8d127ea14fa05f3e
unknown <matthewaomalley@lewisu.edu>
2022-03-02 10:30:33 -0600

    design changes, added functionality

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const app = express()
 // define directory for files to server
 app.use(express.static(__dirname + '/public'))
 
-// need to add routes for other pages to work, but good enough for now
-
 // define the port
 const port = process.env.PORT || 8080;
 
