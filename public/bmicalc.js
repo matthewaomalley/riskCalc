5699bf14e2dd82f540d0e2a0047441abbf4a1b7b
Matthew Senese <matthewjsenese@lewisu.edu>
2022-03-03 12:24:21 -0600

    Fixed some issues

--- public/bmicalc.js
+++
@@ -1,9 +0,0 @@
-// This function will take in parameters both height values
-// and weight and calculate the BMI and return it
-
-// Formula: (weight * 703) / (height in inches^2)
-// rounded by one decimal place
-
-function bmiCalc(bmi, weight, height) {
-    
-}
