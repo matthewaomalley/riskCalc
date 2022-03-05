92f64d86348cf8b4820abe694ef4b4d4a55c2892
Matthew Senese <matthewjsenese@lewisu.edu>
2022-03-05 11:41:59 -0600

    Added BMI function

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ app.use(express.static(__dirname + '/public'))
 // define the port
 const port = process.env.PORT || 8080;
 
+// Function to calculate BMI on the server
+app.get('/calculate-bmi', (request, response) => {
+	var inputs = url.parse(request.url, true).query
+	const heightFeet = parseInt(inputs.feet)
+	const heightInches = parseInt(inputs.inches)
+	const weight = parseInt(inputs.lbs)
+
+	const weightKg = weight*.454
+	const heightM = (heightInches + (heightFeet * 12))*0.0254
+	const BMI = weightKg/(heightM*heightM)
+
+	response.type('text/plain')
+	response.send(BMI.toString())
+})
+
 // listen on the port
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` +
